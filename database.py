d67feffbba82a44eeb8c9a0c05a646d8b8712242
eojin88 <roy2346kk@gmail.com>
2026-04-22 16:57:27 +0900

    "feat: db정보추가"

--- database.py
+++ database.py
@@ -14,6 +14,7 @@ def getConnection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT")),
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor
     )
